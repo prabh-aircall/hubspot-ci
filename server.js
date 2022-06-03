6ea3fa7164c599334113208f18e7ae8ec4a3be00
prabh-aircall <106767777+prabh-aircall@users.noreply.github.com>
2022-06-03 08:11:24 +0530

    initial changes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const base_url = 'https://hubspot-ci.herokuapp.com/';
+
+app.get("/recordings/:externalId", (req, res) => {
+  const { appId, externalAccountId } = req.query;
+  const { externalId } = req.params;
+  console.info({ appId, externalAccountId, externalId });
+  res.send({
+    authenticatedUrl: `${base_url}/audio/${externalId}`,
+  });
+});
+
+app.use("/audio/:externalId", express.static("sample.mp3"));
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
