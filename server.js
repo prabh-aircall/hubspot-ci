71d53d0d0ee0e74547c0ea5de1f9542e81b4d8a4
prabh-aircall <106767777+prabh-aircall@users.noreply.github.com>
2022-06-03 08:22:57 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const base_url = 'https://hubspot-ci.herokuapp.com/';
+const base_url = "https://hubspot-ci.herokuapp.com";
 
 app.get("/recordings/:externalId", (req, res) => {
   const { appId, externalAccountId } = req.query;
