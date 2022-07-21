ecd43ed9170a301055ce592cf6f085140cd3a379
prabh-aircall <106767777+prabh-aircall@users.noreply.github.com>
2022-07-21 08:23:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/recordings/:externalId", (req, res) => {
   });
 });
 
-app.use("/audio/:externalId", express.static("sample.mp3"));
+app.use("/audio/:externalId", express.static("sample1.mp3"));
 
 // This displays message that the server running and listening to specified port
 app.listen(process.env.PORT || port, () =>
